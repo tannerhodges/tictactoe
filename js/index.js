ab6db11c2a1893077694816b88d9861c585ee7f4
Tanner Hodges <austintannerhodges@gmail.com>
2018-02-06 22:45:05 -0500

    Reset button (commented out)

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
 var buttons = document.querySelectorAll("button");
+//var resetButton = document.querySelectorAll("#resetButton");
 var lastClick = "O";
+//var startingBoard = $("#div").html();
 
 function changeButton(event) {
   if (lastClick === "O") {
     event.target.innerText = "X";
+    event.target.style.fontSize = "120px";
     lastClick = "X";
   } else {
     event.target.innerText = "O";
+    event.target.style.fontSize = "120px";
     lastClick = "O";
   }
 
@@ -15,41 +19,37 @@ function changeButton(event) {
 
 function checkForWin() {
   if (buttons[0].innerText === buttons[1].innerText && buttons[1].innerText === buttons[2].innerText) {
-    console.log(buttons[0].innerText + " Wins!");
+    alert(buttons[0].innerText + " Wins!");
   }
   if (buttons[3].innerText === buttons[4].innerText && buttons[4].innerText === buttons[5].innerText) {
-    console.log(buttons[3].innerText + " Wins!");
+    alert(buttons[3].innerText + " Wins!");
   }
   if (buttons[6].innerText === buttons[7].innerText && buttons[7].innerText === buttons[8].innerText) {
-    console.log(buttons[6].innerText + " Wins!");
+    alert(buttons[6].innerText + " Wins!");
   }
   if (buttons[0].innerText === buttons[3].innerText && buttons[3].innerText === buttons[6].innerText) {
-    console.log(buttons[0].innerText + " Wins!");
+    alert(buttons[0].innerText + " Wins!");
   }
   if (buttons[1].innerText === buttons[4].innerText && buttons[4].innerText === buttons[7].innerText) {
-    console.log(buttons[1].innerText + " Wins!");
+    alert(buttons[1].innerText + " Wins!");
   }
   if (buttons[2].innerText === buttons[5].innerText && buttons[5].innerText === buttons[8].innerText) {
-    console.log(buttons[2].innerText + " Wins!");
+    alert(buttons[2].innerText + " Wins!");
   }
   if (buttons[0].innerText === buttons[4].innerText && buttons[4].innerText === buttons[8].innerText) {
-    console.log(buttons[0].innerText + " Wins!");
+    alert(buttons[0].innerText + " Wins!");
   }
   if (buttons[2].innerText === buttons[4].innerText && buttons[4].innerText === buttons[6].innerText) {
-    console.log(buttons[2].innerText + " Wins!");
+    alert(buttons[2].innerText + " Wins!");
   }
 }
-
-// buttons[0].addEventListener("click", changeButton);
-// buttons[1].addEventListener("click", changeButton);
-// buttons[2].addEventListener("click", changeButton);
-// buttons[3].addEventListener("click", changeButton);
-// buttons[4].addEventListener("click", changeButton);
-// buttons[5].addEventListener("click", changeButton);
-// buttons[6].addEventListener("click", changeButton);
-// buttons[7].addEventListener("click", changeButton);
-// buttons[8].addEventListener("click", changeButton);
 
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", changeButton);
 }
+
+//function resetBoard(){
+//  $("#div").html(startingBoard);
+//}
+//
+//resetbutton.addEventListener("click", resetBoard);
